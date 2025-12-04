cf588e25c52ad8a089bc79fd07425e448cb9e1f4
sulatt3 <su.h.latt3@gmail.com>
2025-12-04 07:59:02 -0500

    Create __init__.py

---
+++ modules/competitive/__init__.py
@@ -0,0 +1,16 @@
+"""Competitive Intelligence Module"""
+```
+
+4. **Scroll down, add commit message:** `Add competitive module structure`
+
+5. **Click "Commit new file"**
+
+6. **Repeat for these files (one at a time):**
+```
+   modules/segmint/__init__.py # Coming soon
+   modules/campaigns/__init__.py # Coming soon
+   modules/__init__.py # Coming soon
+   orchestrator.py # Coming soon
+   streamlit_app.py # Coming soon
+   requirements.txt
+   .env.example
